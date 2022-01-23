33a13732e855639937ae30ce7815ce82effa45d9
Kittitat Chainonthi <kittitat.chainonthi@gmail.com>
2022-01-23 15:20:06 +0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="test-temp text-center text-6xl">ฮัลโหล<span> KOTCHA</span></h1>
   </div>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+* {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.test-temp {
+  color: red;
+  font-weight: 900;
+  span {
+    @apply text-black;
+  }
+}
+</style>
